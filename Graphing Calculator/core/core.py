a060271bf989071b3ca08bfc62bd2a6c86a14dd3
Monika <devindematto@gmail.com>
2020-02-04 15:05:58 -0500

    Adding Widget Position Check

--- Graphing Calculator/core/core.py
+++ Graphing Calculator/core/core.py
@@ -1,5 +1,6 @@
 from kivy.app import App
-from kivy.properties import DictProperty, ListProperty, ObjectProperty
+from kivy.clock import Clock
+from kivy.properties import DictProperty, ObjectProperty, NumericProperty
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.label import Label
 from kivy.uix.recycleview import RecycleView
@@ -13,47 +14,28 @@ class Main(FloatLayout):
 
 
 class Graph(FloatLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.axis_y = AxisY(size_hint=[1, 1], ctx=self)
-        self.axis_x = AxisX(size_hint=[1, 1], ctx=self)
-        self.add_widget(self.axis_x)
-        self.add_widget(self.axis_y)
-
-        self.markers_count = 4
-        self.mark_distance = 1 / self.markers_count
-        self.markers_x = []
-        self.markers_y = []
-        self.mark_init()
-
-    def mark_init(self):
-        self.mark_gen('x')
-        self.mark_gen('y')
-
-    def mark_gen(self, axis):
-        for mark in range(self.markers_count):
-            marker = Marker()
-            if axis == 'x':
-                marker.size_hint = [.01, .02]
-                marker.pos_hint = {'x': self.mark_distance * mark + .1, 'y': self.axis_x.pos_hint['x'] - .007}
-                self.markers_x.append(marker)
-                self.axis_x.add_widget(marker)
-            elif axis == 'y':
-                marker.size_hint = [.02, .01]
-                marker.pos_hint = {'x': self.axis_y.pos_hint['y'] - .007, 'y': self.mark_distance * mark + .1}
-                self.markers_y.append(marker)
-                self.axis_y.add_widget(marker)
+    pass
 
 
 class AxisX(FloatLayout):
     pos_hint = DictProperty({'x': 0, 'y': 0.5})
-    markers = ListProperty()
-    y_axis = ObjectProperty()
 
-    def __init__(self, ctx: Graph, **kwargs):
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.y_axis = ctx.axis_y
-        print(self.y_axis.y)
+        self.markers_count = 5
+        self.mark_distance = 1 / self.markers_count
+
+        Clock.schedule_once(self.mark_gen, 1)
+
+    def mark_gen(self, dt):
+        mark_values = {-2: -300, -1: -150, 1: 130, 2: 300}
+        for key, value in mark_values.items():
+            marker = MarkerX(self)
+            marker.text = str(key)
+            marker.size_hint = [.01, .02]
+            marker.x = value
+            marker.pos_hint['y'] = self.pos_hint['x']
+            self.add_widget(marker)
 
     def on_touch_move(self, touch):
         if self.parent.collide_point(*touch.pos):
@@ -64,14 +46,33 @@ class AxisX(FloatLayout):
                 self.pos_hint['y'] -= abs(speed * touch.dy)
 
 
+class MarkerX(Label):
+    axis_y = ObjectProperty(rebind=True)
+
+    def __init__(self, ctx: AxisX, **kwargs):
+        super().__init__(**kwargs)
+        self.ctx = ctx
+        self.axis_y = self.ctx.parent.ids.axis_y
+
+
+
 class AxisY(FloatLayout):
     pos_hint = DictProperty({'x': 0.5, 'y': 0})
-    markers = ListProperty()
-    x_axis = ObjectProperty()
 
-    def __init__(self, ctx: Graph, **kwargs):
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        print(ctx.axis_x)
+
+        self.markers_count = 4
+        self.mark_distance = 1 / self.markers_count
+
+        Clock.schedule_once(self.mark_gen, 1)
+
+    def mark_gen(self, dt):
+        for mark in range(self.markers_count):
+            marker = MarkerY(self)
+            marker.size_hint = [.02, .01]
+            marker.pos_hint = {'x': self.pos_hint['y'] - .007, 'y': self.mark_distance * mark - .4}
+            self.add_widget(marker)
 
     def on_touch_move(self, touch):
         if self.parent.collide_point(*touch.pos):
@@ -82,8 +83,13 @@ class AxisY(FloatLayout):
                 self.pos_hint['x'] -= abs(speed * touch.dx)
 
 
-class Marker(Label):
-    pass
+class MarkerY(Label):
+    axis_x = ObjectProperty(rebind=True)
+
+    def __init__(self, ctx: AxisY, **kwargs):
+        super().__init__(**kwargs)
+        self.ctx = ctx
+        self.axis_x = self.ctx.parent.ids.axis_x
 
 
 class SideBar(FloatLayout):
